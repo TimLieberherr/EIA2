7e28eece899e0e079f37efc927941e2727aeec94
Tim Lieberherr <tim.lieberherr@hs-furtwangen.de>
2018-03-29 12:08:38 +0200

    Verbesserung ts

--- Aufgabe 0/Hallo_Name.js
+++ Aufgabe 0/Hallo_Name.js
@@ -1,10 +1,10 @@
 /*
 Name: Tim Lieberherr
 Matrikel: (257969)
-Datum: (25.03.18) Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/ 
-
+Datum: (25.03.18) Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
 function hallo() {
-  var name = prompt("Hey, wie ist dein Name?", "");
-    document.getElementById("Hallo_Name").innerHTML =
-      "Hey " + name + ". Schön das du hier bist.";  
+    var hey = prompt("Hey, wie ist dein Name?", "");
+    var container = document.getElementById("Hallo_Name");
+    container.innerHTML = "Hey " + hey + ". Schön das du hier bist.";
 }
+//# sourceMappingURL=Hallo_Name.js.map
--- Aufgabe 0/Hallo_Name.ts
+++ Aufgabe 0/Hallo_Name.ts
@@ -1,10 +1,10 @@
 /*
 Name: Tim Lieberherr
 Matrikel: (257969)
-Datum: (25.03.18) Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/ 
+Datum: (25.03.18) Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
 
-function hallo() {
-  var hey = prompt("Hey, wie ist dein Name?", "");
-   var container = document.getElementById("Hallo_Name");
-    container.innerHTML = "Hey " + hey + ". Schön das du hier bist.";  
+function hallo(): void {
+    var hey: string = prompt("Hey, wie ist dein Name?", "");
+    var container: HTMLElement = document.getElementById("Hallo_Name");
+    container.innerHTML = "Hey " + hey + ". Schön das du hier bist.";
 }
