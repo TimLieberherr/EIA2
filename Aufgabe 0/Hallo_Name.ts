77a51105e5e5cb0761a4ce04403da73fbbb45730
Tim Lieberherr <tim.lieberherr@hs-furtwangen.de>
2018-03-29 11:41:46 +0200

    ts linter

--- Aufgabe 0/Hallo_Name.ts
+++ Aufgabe 0/Hallo_Name.ts
@@ -4,7 +4,7 @@ Matrikel: (257969)
 Datum: (25.03.18) Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/ 
 
 function hallo() {
-  var name = prompt("Hey, wie ist dein Name?", "");
-    document.getElementById("Hallo_Name").innerHTML =
-      "Hey " + name + ". Schön das du hier bist.";  
+  var hey = prompt("Hey, wie ist dein Name?", "");
+   var container = document.getElementById("Hallo_Name");
+    container.innerHTML ="Hey " + hey + ". Schön das du hier bist.";  
 }
