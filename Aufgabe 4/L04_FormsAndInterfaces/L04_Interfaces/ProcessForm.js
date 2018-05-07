484a897d621439bd1b7ef688c5496a2962e4c236
Tim Lieberherr <tim.lieberherr@hs-furtwangen.de>
2018-05-07 15:26:45 +0200

    ProcessForms

--- Aufgabe 4/L04_FormsAndInterfaces/L04_Interfaces/ProcessForm.js
+++ Aufgabe 4/L04_FormsAndInterfaces/L04_Interfaces/ProcessForm.js
@@ -5,36 +5,16 @@ var L04_Interfaces;
         console.log("Init");
         var insertButton = document.getElementById("insert");
         var refreshButton = document.getElementById("refresh");
-        var searchButton = document.getElementById("searchbutton");
+        var searchButton = document.getElementById("searchButton");
         insertButton.addEventListener("click", insert);
         refreshButton.addEventListener("click", refresh);
         searchButton.addEventListener("click", search);
-    }
-    function search(_event) {
-        var output = document.getElementsByTagName("textarea")[0];
-        var matrikel = parseInt(document.getElementById("searchInput").value);
-        output.value = "";
-        for (var matrikel_1 in L04_Interfaces.studiHomoAssoc) {
-            var studi = L04_Interfaces.studiHomoAssoc[matrikel_1];
-            if (studi == undefined)
-                output.value += "Kein Suchergebnis gefunden";
-            else {
-                var line = matrikel_1 + ": ";
-                line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre, ";
-                line += studi.studyPath + ", ";
-                line += studi.gender ? "männlich" : "weiblich";
-                output.value += line + "\n";
-            }
-        }
-        // gefunden: gib den studenten aus steht in der let studi
-        // student: studi.name, studi.age,           
     }
     function insert(_event) {
         var inputs = document.getElementsByTagName("input");
         var genderButton = document.getElementById("male");
         var studyPath = document.getElementById("select");
         var matrikel = inputs[2].value;
-        console.log(inputs);
         var studi;
         studi = {
             name: inputs[0].value,
@@ -47,28 +27,43 @@ var L04_Interfaces;
         console.log(studi);
         console.log(studi.age);
         console.log(studi["age"]);
-        // Datensatz im assoziativen Array unter der Matrikelnummer speichern
         L04_Interfaces.studiHomoAssoc[matrikel] = studi;
-        // nur um das auch noch zu zeigen...
         L04_Interfaces.studiSimpleArray.push(studi);
     }
     function refresh(_event) {
-        var output = document.getElementsByTagName("textarea")[1];
+        var output = document.getElementsByTagName("textarea")[0];
         output.value = "";
         for (var matrikel in L04_Interfaces.studiHomoAssoc) {
             var studi = L04_Interfaces.studiHomoAssoc[matrikel];
             var line = matrikel + ": ";
             line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
-            line += studi.gender ? "(M)" : "(F)";
+            line += studi.studyPath + ", ";
+            line += studi.gender ? "männlich" : "weiblich";
             output.value += line + "\n";
         }
-        // zusätzliche Konsolenausgaben zur Demonstration
-        console.group("Simple Array");
-        console.log(L04_Interfaces.studiSimpleArray);
-        console.groupEnd();
-        console.group("Associatives Array (Object)");
-        console.log(L04_Interfaces.studiHomoAssoc);
-        console.groupEnd();
     }
+    function search(_event) {
+        var output = document.getElementById("textarea2");
+        output.value = "";
+        var matrikel = parseInt(document.getElementById("matrikelNr").value);
+        var studi = L04_Interfaces.studiHomoAssoc[matrikel];
+        if (typeof studi === "undefined") {
+            output.value += "Kein Suchergebnis gefunden";
+        }
+        else {
+            var line = matrikel + ": ";
+            line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre, ";
+            line += studi.studyPath + ", ";
+            line += studi.gender ? "männlich" : "weiblich";
+            output.value += line + "\n";
+        }
+    }
+    // zusätzliche Konsolenausgaben zur Demonstration
+    console.group("Simple Array");
+    console.log(L04_Interfaces.studiSimpleArray);
+    console.groupEnd();
+    console.group("Associatives Array (Object)");
+    console.log(L04_Interfaces.studiHomoAssoc);
+    console.groupEnd();
 })(L04_Interfaces || (L04_Interfaces = {}));
 //# sourceMappingURL=ProcessForm.js.map
--- Aufgabe 4/L04_FormsAndInterfaces/L04_Interfaces/ProcessForm.ts
+++ Aufgabe 4/L04_FormsAndInterfaces/L04_Interfaces/ProcessForm.ts
@@ -1,49 +1,29 @@
 namespace L04_Interfaces {
+    
     window.addEventListener("load", init);
 
     function init(_event: Event): void {
         console.log("Init");
         let insertButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("insert");
         let refreshButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("refresh");
-        let searchButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("searchbutton");
+        let searchButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("searchButton");
+        
         insertButton.addEventListener("click", insert);
         refreshButton.addEventListener("click", refresh);
         searchButton.addEventListener("click", search);
-    }
-    function search(_event: Event): void {
-        let output: HTMLTextAreaElement = document.getElementsByTagName("textarea")[0];
-        let matrikel: number = parseInt((<HTMLInputElement>document.getElementById("searchInput")).value);
-        output.value = "";
-
-        for (let matrikel in studiHomoAssoc) {
-            let studi: Studi = studiHomoAssoc[matrikel];
-            if (studi == undefined)
-                output.value += "Kein Suchergebnis gefunden";
-            // nicht gefunden
-            // return;
-            else {
-                let line: string = matrikel + ": ";
-                line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre, ";
-                line += studi.studyPath + ", ";
-                line += studi.gender ? "männlich" : "weiblich";
-                output.value += line + "\n";
-            }
-        }
-        // gefunden: gib den studenten aus steht in der let studi
-        // student: studi.name, studi.age,           
     }
 
     function insert(_event: Event): void {
         let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
         let genderButton: HTMLInputElement = <HTMLInputElement>document.getElementById("male");
-        let studyPath: HTMLSelectElement = <HTMLSelectElement>document.getElementById("select");
+        let studyPath: HTMLSelectElement = <HTMLSelectElement>document.getElementById("select");  
         let matrikel: string = inputs[2].value;
-        console.log(inputs);
         let studi: Studi;
+        
         studi = {
             name: inputs[0].value,
             firstname: inputs[1].value,
-            studyPath: studyPath.value,
+            studyPath: studyPath.value,          
             matrikel: parseInt(matrikel),
             age: parseInt(inputs[3].value),
             gender: genderButton.checked
@@ -53,31 +33,51 @@ namespace L04_Interfaces {
         console.log(studi.age);
         console.log(studi["age"]);
 
-        // Datensatz im assoziativen Array unter der Matrikelnummer speichern
         studiHomoAssoc[matrikel] = studi;
 
-        // nur um das auch noch zu zeigen...
         studiSimpleArray.push(studi);
     }
 
     function refresh(_event: Event): void {
-        let output: HTMLTextAreaElement = document.getElementsByTagName("textarea")[1];
+        let output: HTMLTextAreaElement = document.getElementsByTagName("textarea")[0];
         output.value = "";
-        for (let matrikel in studiHomoAssoc) {
+        
+        for (let matrikel in studiHomoAssoc) {  
             let studi: Studi = studiHomoAssoc[matrikel];
             let line: string = matrikel + ": ";
             line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
-            line += studi.gender ? "(M)" : "(F)";
+            line += studi.studyPath + ", ";                            
+            line += studi.gender ? "männlich" : "weiblich";
             output.value += line + "\n";
         }
-
-        // zusätzliche Konsolenausgaben zur Demonstration
-        console.group("Simple Array");
-        console.log(studiSimpleArray);
-        console.groupEnd();
-
-        console.group("Associatives Array (Object)");
-        console.log(studiHomoAssoc);
-        console.groupEnd();
     }
+
+    function search(_event: Event): void {
+        let output: HTMLTextAreaElement = <HTMLTextAreaElement>document.getElementById("textarea2");
+        output.value = "";
+        let matrikel: number = parseInt((<HTMLInputElement>document.getElementById("matrikelNr")).value);
+        let studi: Studi = studiHomoAssoc[matrikel];
+        
+        if (typeof studi === "undefined") {
+            output.value += "Kein Suchergebnis gefunden";
+        }
+        
+        else {
+            let line: string = matrikel + ": ";
+            line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre, ";
+            line += studi.studyPath + ", ";                            
+            line += studi.gender ? "männlich" : "weiblich";
+            output.value += line + "\n";
+        }
+    
+    }
+
+    // zusätzliche Konsolenausgaben zur Demonstration
+    console.group("Simple Array");
+    console.log(studiSimpleArray);
+    console.groupEnd();
+
+    console.group("Associatives Array (Object)");
+    console.log(studiHomoAssoc);
+    console.groupEnd();
 }
